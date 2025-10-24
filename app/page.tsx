c5602e6ccc964949ab8d7bdb1f973b9dfb04c918
shaktisingh647 <consolelogshakti@gmail.com>
2025-10-24 17:09:00 +0530

    favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button"
 function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       page
-      
+      <Button>Click Me</Button>
     </div>
   )
 }
