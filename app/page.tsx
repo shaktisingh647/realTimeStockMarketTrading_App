9bc2a9a4bd6db5e4e5b835c6811633068daf72f2
shaktisingh647 <consolelogshakti@gmail.com>
2025-10-28 11:57:15 +0530

    newupdation

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       page
-      <Button>Click Me and</Button>
+      <Button>Click Me</Button>
       
     </div>
   )
