65e69e608b1df3430d2ec382f90e76beeefd7549
shaktisingh647 <consolelogshakti@gmail.com>
2025-10-29 09:41:36 +0530

    editing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      page
+      
       <Button>Click Me</Button>
       
     </div>
